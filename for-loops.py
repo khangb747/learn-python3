6c351aecfa8eafc3231ebccc5b05d758a681d8a6
felix <134663102+khangb747@users.noreply.github.com>
2023-07-08 09:06:59 +0700

    learn-python3

---
+++ for-loops.py
@@ -0,0 +1,8 @@
+numbers = [5, 7, 9, 11, 12]
+for item in numbers:
+    print(item)
+
+i = 0
+while i < len(numbers):
+    print(numbers[i])
+    i = i + 1
---
+++ the-range()-function.py
@@ -0,0 +1,3 @@
+numbers = range(7, 28, 4)
+for number in range(7):
+    print(number)
